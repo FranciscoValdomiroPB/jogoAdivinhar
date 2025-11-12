b118d4787c4ad31024501f33c2980f9ff4313b77
FranciscoValdomiroPB <valdopinto000000@gmail.com>
2025-11-11 21:58:34 -0400

    salvando as alterações

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Random gerador = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int numeroAleatorio = gerador.nextInt(100);
+        System.out.println("Adivinhe um numero que estou pensando entre 0 e 100:");
+        int numeroUsuario = scanner.nextInt();
+
+        if (numeroAleatorio == numeroUsuario) {
+            System.out.println("Parabéns! Você adivinhou o número!");
+        } else {
+            System.out.println("Que pena! O número era: " + numeroAleatorio);            
+        }
+
+        scanner.close();
+
+
     }
 }
